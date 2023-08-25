9564225bca9e820549febd95107c282104daca40
magdalena krasicka <krasickamagdalena@gmail.com>
2023-08-25 09:35:27 +0100

    completed rock paper scissors

--- game.js
+++ game.js
@@ -1,128 +1,85 @@
 // Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
-const hands =[... document.querySelectorAll('.select img')];
-const btn = document.querySelector('button');
-const panelRight = [...document.querySelectorAll('.panel-right p')]
-const panelLeft = [...document.querySelectorAll('.panel-left p')]
 
-
-const gameSummary = {
-  number: 0,
-  wins: 0,
-  losses: 0,
-  draws: 0
-}
+const hands = document.querySelectorAll(".select img");
+const playButton = document.querySelector("button.start");
+const divRight = document.querySelector("div.panel-right");
 
 const game = {
   playerHand: "",
-  aiHand: ""
-}
-
-
-
-// Funtion 1
-function handSelection(e) {
-  // console.log(this.dataset);
-  game.playerHand = this.dataset.option;
-  hands.forEach((hand) => hand.style.boxShadow = "");
-  this.style.boxShadow = "0 0 0 4px red";
-
+  aiHand: "",
+  winner: ""
 };
 
-function aiChoice() {
-  return hands[Math.floor(Math.random() * hands.length)].dataset.option;
+const gameSummary = {
+  gameNumber: 0,
+  wins: 0,
+  loses: 0,
+  draw: 0
+};
+
+const handSelection = function(e) {
+  game.playerHand = this.dataset.option;
+
+  hands.forEach((hand) => {
+    hand.style.boxShadow = ""
+  })
+  this.style.boxShadow = "0 0 0 4px yellow";
 }
-
-function checkResult(player, ai) {
-  if (player === ai) {
-    alert("remis!");
-
-    return "draw"
-  } else if (player === "papier" && ai === "kamień" || player === "kamień" && ai === "nożyczki" || player === "nożyczki" && ai === "papier") {
-    alert("Wygrales!");
-
-    return "win"
-  } else {
-    alert("Przegrales!");
-
-    return "loss"
-  }
-}
-
-function publishResult(player, ai, result) {
-  document.querySelector('[data-summary="your-choice"]').textContent = player;
-
-  document.querySelector('[data-summary="ai-choice"]').textContent = ai;
-
-  document.querySelector('p.numbers span').textContent = ++gameSummary.number;
-
-  if (result === "win") {
-
-    document.querySelector('p.wins span').textContent = ++gameSummary.wins
-    document.querySelector('[data-summary= "who-win"]').textContent = "You!"
-    document.querySelector('[data-summary= "who-win"]').style.color = "green"
-
-  } else if (result === "loss") {
-
-    document.querySelector('[data-summary= "who-win"]').textContent = "AI!"
-    document.querySelector('p.losses span').textContent = ++gameSummary.losses
-    document.querySelector('[data-summary= "who-win"]').style.color = "red"
-
-  } else {
-
-    document.querySelector('[data-summary= "who-win"]').textContent = "Remis!"
-    document.querySelector('p.draws span').textContent = ++gameSummary.draws;
-    document.querySelector('[data-summary= "who-win"]').style.color = "gray"
-
-  }
-}
-
-function endGame() {
-
-
-  document.querySelector(`[data-option= "${game.playerHand}"]`).style.boxShadow = "";
-
-  game.playerHand = "";
-  game.aiHand = "";
-}
-
-// Function 2
-function letsPlay(e) {
-  if (!game.playerHand) {
-    return alert("Choose hand!");
-  }
-
-  game.aiHand = aiChoice();
-
-  const gameResult = checkResult(game.playerHand, game.aiHand);
-
-  publishResult(game.playerHand, game.aiHand, gameResult);
-
-  endGame();
-  // update twoj wybor
-  // update wybor komputera
-  // zwyciezca gry
-
-
-}
-
 
 
 hands.forEach((hand) => {
-  hand.addEventListener("click", handSelection)
+  hand.style.boxShadow = "";
+  hand.addEventListener("click", handSelection);
 });
 
-btn.addEventListener('click', letsPlay);
 
 
+function checkResult(playerHand, aiHand) {
+  if (playerHand === aiHand) {
+    return "Draw"
+  } else if (playerHand === "papier" && aiHand === "kamień" || playerHand == "kamień" && aiHand === "nożyczki" || playerHand == "nożyczki" && aiHand === "papier"){
+    return "Win"
+  } else {
+    return "Loss"
+  }
+};
 
 
+function publishResult(player, ai, result) {
+  const h2 = document.querySelector('span[data-summary ="who-win"]');
+  h2.textContent = result;
 
+  const yourChoice = document.querySelector('span[data-summary="your-choice"]');
+  yourChoice.innerHTML = player;
+  const aiChoice = document.querySelector('span[data-summary="ai-choice"]');
+  aiChoice.innerHTML = ai;
+  document.querySelector('p.numbers span').textContent = ++gameSummary.gameNumber;
 
-// const handSelection = (e) => {
-//   // this odnosi sie do obiektu globalnego window
-//   // console.log(this);
+  if (result === "Win") {
+    game.winner = "You won"
+    h2.style.color = "green";
+    document.querySelector('p.wins span').textContent = ++gameSummary.wins
+  } else if (result === "Loss") {
+    game.winner = "AI won"
+    h2.style.color = "red";
+    document.querySelector('p.losses span').textContent = ++gameSummary.loses
+  } else {
+    game.winner = "Remis"
+    h2.style.color = "gray";
+    document.querySelector('p.draws span').textContent = ++gameSummary.draw
+  }
+}
 
-//   // all below is the same
-//   // console.log(e.currentTarget);
-//   // console.log(e.target);
-// }
+const startGame = function() {
+if (game.playerHand === "") {
+  alert ("Choose hand!")
+  return
+}
+  const options = ["papier", "kamień", "nożyczki"];
+  const index = Math.floor(Math.random() * 3);
+  game.aiHand = options[index];
+  const gameResult = checkResult(game.playerHand, game.aiHand);
+  publishResult(game.playerHand, game.aiHand, gameResult);
+}
+
+playButton.addEventListener('click', startGame);
